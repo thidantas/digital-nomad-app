09d0989b595c5e341bd857f48b0cc52b36a38eeb
thidantas <eng.thiagohpdantas@gmail.com>
2026-08-04 13:34:37 -0300

    feat(home): debounce city search input with useDebounce

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { Screen } from "@/src/components/Screen";
 import { CityFilter } from "@/src/containers/CityFilter";
 import { categories } from "@/src/data/categories";
 import { useCities } from "@/src/data/useCities";
+import { useDebounce } from "@/src/hooks/useDebounce";
 import { useAppTheme } from "@/src/theme/useAppTheme";
 import { CityPreview } from "@/src/types";
 import { useScrollToTop } from "@react-navigation/native";
@@ -15,11 +16,13 @@ export default function HomeScreen() {
   const { spacing } = useAppTheme();
   const { top } = useSafeAreaInsets();
   const [cityName, setCityName] = useState("");
+
+  const debouncedCityName = useDebounce(cityName);
   const [selectedCategoryId, setSelectedCategoryId] = useState<string | null>(
     null,
   );
 
-  const { cityPreviewList } = useCities(cityName, selectedCategoryId);
+  const { cityPreviewList } = useCities(debouncedCityName, selectedCategoryId);
 
   const flatListRef = useRef(null);
   useScrollToTop(flatListRef);
