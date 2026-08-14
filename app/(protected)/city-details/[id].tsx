5295e976c772012dd3859a9c7d00219f7ab41a75
thidantas <eng.thiagohpdantas@gmail.com>
2026-08-14 18:55:17 -0300

    feat(city-details): add MapView section with city location

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -41,7 +41,7 @@ export default function CityDetails() {
       />
 
       <Divider paddingHorizontal="padding" />
-      <CityDetailsMap />
+      <CityDetailsMap location={city.location} />
 
       <Divider paddingHorizontal="padding" />
 
