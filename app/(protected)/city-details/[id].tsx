a74876c85c3b735e82eff7b8f77062dfc79ae019
thidantas <eng.thiagohpdantas@gmail.com>
2026-08-15 12:48:36 -0300

    feat(city-details): open expanded map in animated BottomSheet

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -1,4 +1,10 @@
+import { useLocalSearchParams } from "expo-router";
+import { Pressable } from "react-native";
+import MapView from "react-native-maps";
+import { useSharedValue } from "react-native-reanimated";
+
 import { Divider } from "@/src/components";
+import { BottomSheet } from "@/src/components/BottomSheet";
 import { Screen } from "@/src/components/Screen";
 import { Text } from "@/src/components/Text";
 import { CityDetailsHeader } from "@/src/containers/CityDetailsHeader";
@@ -7,11 +13,15 @@ import { CityDetailsMap } from "@/src/containers/CityDetailsMap";
 import { CityDetailsRelatedCities } from "@/src/containers/CityDetailsRelatedCities";
 import { CityDetailsTouristAttractions } from "@/src/containers/CityDetailsTouristAttractions";
 import { useCityDetails } from "@/src/data/useCityDetails";
-import { useLocalSearchParams } from "expo-router";
 
 export default function CityDetails() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const city = useCityDetails(id);
+
+  const bottomSheetIsOpen = useSharedValue(false);
+  function toggleBottomSheet() {
+    bottomSheetIsOpen.value = !bottomSheetIsOpen.value;
+  }
 
   if (!city) {
     return (
@@ -22,30 +32,50 @@ export default function CityDetails() {
   }
 
   return (
-    <Screen style={{ paddingHorizontal: 0 }} scrollable>
-      <CityDetailsHeader
-        id={city.id}
-        coverImage={city.coverImage}
-        categories={city.categories}
-      />
-      <CityDetailsInfo
-        name={city.name}
-        country={city.country}
-        description={city.description}
-      />
+    <>
+      <Screen style={{ paddingHorizontal: 0 }} scrollable>
+        <CityDetailsHeader
+          id={city.id}
+          coverImage={city.coverImage}
+          categories={city.categories}
+        />
+        <CityDetailsInfo
+          name={city.name}
+          country={city.country}
+          description={city.description}
+        />
 
-      <Divider paddingHorizontal="padding" />
+        <Divider paddingHorizontal="padding" />
 
-      <CityDetailsTouristAttractions
-        touristAttractions={city.touristAttractions}
-      />
+        <CityDetailsTouristAttractions
+          touristAttractions={city.touristAttractions}
+        />
 
-      <Divider paddingHorizontal="padding" />
-      <CityDetailsMap location={city.location} />
+        <Divider paddingHorizontal="padding" />
 
-      <Divider paddingHorizontal="padding" />
+        <Pressable onPress={toggleBottomSheet}>
+          <CityDetailsMap location={city.location} />
+        </Pressable>
 
-      <CityDetailsRelatedCities />
-    </Screen>
+        <Divider paddingHorizontal="padding" />
+
+        <CityDetailsRelatedCities />
+
+        <BottomSheet onPress={toggleBottomSheet} isOpen={bottomSheetIsOpen}>
+          <MapView
+            style={{
+              width: "100%",
+              height: 500,
+            }}
+            initialRegion={{
+              latitude: city.location.latitude,
+              longitude: city.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </BottomSheet>
+      </Screen>
+    </>
   );
 }
---
+++ src/components/BottomSheet.tsx
@@ -0,0 +1,74 @@
+import { PropsWithChildren } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+type BottomSheetProps = {
+  onPress: () => void;
+  isOpen: SharedValue<boolean>;
+  duration?: number;
+};
+
+export function BottomSheet({
+  children,
+  onPress,
+  isOpen,
+  duration = 600,
+}: PropsWithChildren<BottomSheetProps>) {
+  const height = useSharedValue(0);
+
+  const progress = useDerivedValue(() =>
+    withTiming(isOpen.value ? 0 : 1, { duration }),
+  );
+
+  const sheetAnimatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      // initial height: 500
+
+      // progress 0 | height 0
+      // progress 0.25 | height 125
+      // progress 0.5 | height 250
+      //
+      { translateY: progress.value * height.value },
+    ],
+    zIndex: 2,
+  }));
+
+  const backdropAnimatedStyle = useAnimatedStyle(() => ({
+    zIndex: isOpen.value ? 1 : -1,
+  }));
+
+  return (
+    <>
+      <Animated.View style={[styles.backdrop, backdropAnimatedStyle]}>
+        <TouchableOpacity style={{ flex: 1 }} onPress={onPress} />
+      </Animated.View>
+
+      <Animated.View
+        style={[styles.sheet, sheetAnimatedStyle]}
+        onLayout={(e) => {
+          height.value = e.nativeEvent.layout.height;
+        }}
+      >
+        {children}
+      </Animated.View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  sheet: {
+    position: "absolute",
+    width: "100%",
+    bottom: 0,
+  },
+  backdrop: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,0.7)",
+  },
+});
