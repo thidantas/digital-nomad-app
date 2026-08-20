273bf2936dcb6e572f4a7d57577eda96134c3caa
Thiago Dantas <eng.thiagohpdantas@gmail.com>
2026-08-20 14:35:31 -0300

    feat(ui): add Button component and wire it on sign-in

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,8 +1,8 @@
 import { useAuthSignIn } from "@/src/domain/auth/operations/useAuthSignIn";
+import { Button } from "@/src/ui/components/Button";
 import { Screen } from "@/src/ui/components/Screen";
 import { TextInput } from "@/src/ui/components/TextInput";
 import { useState } from "react";
-import { Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SignInScreen() {
@@ -32,7 +32,7 @@ export default function SignInScreen() {
           onChangeText={setPassword}
           placeholder="digite sua senha"
         />
-        <Button title="Entrar" onPress={handleSignIn} />
+        <Button title="Entrar" mt="s20" onPress={handleSignIn} />
       </SafeAreaView>
     </Screen>
   );
