66983ac13ecc05966bd36079bab6bae3bc5f3ef6
thidantas <eng.thiagohpdantas@gmail.com>
2026-08-22 17:59:12 -0300

    feat(auth): complete SignUpForm with validated input fields

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -3,12 +3,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Logo } from "@/src/ui/components/Logo";
 import { Screen } from "@/src/ui/components/Screen";
 import { SignUpForm } from "@/src/ui/containers/Forms/SignUpForm/SignUpForm";
+import { SignUpSchema } from "@/src/ui/containers/Forms/SignUpForm/SignUpSchema";
 import { Header } from "@/src/ui/containers/Header";
 
 export default function SignUpScreen() {
-  function handleSignUp() {
+  function handleSignUp(data: SignUpSchema) {
+    console.log(data);
     //
   }
+
   return (
     <Screen>
       <SafeAreaView>
