76bf3a845088b109f4ec841499e4d6678d079ed6
thidantas <eng.thiagohpdantas@gmail.com>
2026-08-22 18:48:54 -0300

    feat(auth): wire sign-up through useAuthSignUp and in-memory repo

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,15 +1,22 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { useAuthSignUp } from "@/src/domain/auth/operations/useAuthSignUp";
 import { Logo } from "@/src/ui/components/Logo";
 import { Screen } from "@/src/ui/components/Screen";
 import { SignUpForm } from "@/src/ui/containers/Forms/SignUpForm/SignUpForm";
 import { SignUpSchema } from "@/src/ui/containers/Forms/SignUpForm/SignUpSchema";
 import { Header } from "@/src/ui/containers/Header";
+import { router } from "expo-router";
 
 export default function SignUpScreen() {
-  function handleSignUp(data: SignUpSchema) {
-    console.log(data);
-    //
+  const { mutate: signUp } = useAuthSignUp({ onSuccess: router.back });
+
+  function handleSignUp(formValues: SignUpSchema) {
+    signUp({
+      email: formValues.email,
+      fullname: formValues.fullname,
+      password: formValues.password,
+    });
   }
 
   return (
---
+++ src/domain/auth/operations/useAuthSignUp.ts
@@ -0,0 +1,27 @@
+import {
+  useAppMutation,
+  UseAppMutationOptions,
+} from "@/src/infra/operations/useAppMutation";
+import { useRepository } from "@/src/infra/repositories/RepositoryProvider";
+import { useFeedbackService } from "@/src/services/feedbackService/FeedbackProvider";
+import { AuthSignUpParams } from "../IAuthRepo";
+
+export function useAuthSignUp(options?: UseAppMutationOptions<void>) {
+  const { auth } = useRepository();
+  const feedbackService = useFeedbackService();
+
+  return useAppMutation<void, AuthSignUpParams>({
+    mutateFn: (params) => auth.signUp(params),
+    onSuccess: () => {
+      options?.onSuccess?.();
+      feedbackService.send({
+        type: "success",
+        message: `cadastro feito com sucesso`,
+      });
+    },
+    onError: (error) => {
+      options?.onError?.(error);
+      feedbackService.send({ type: "error", message: "erro ao cadastras" });
+    },
+  });
+}
