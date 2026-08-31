49fa87376cc6f2a9a03f4e8eb238ad2b411794ff
thidantas <eng.thiagohpdantas@gmail.com>
2026-08-31 16:26:12 -0300

    feat(auth): sign in with TanStack Query mutation

--- src/domain/auth/operations/useAuthSignIn.ts
+++ src/domain/auth/operations/useAuthSignIn.ts
@@ -1,6 +1,6 @@
-import { useAppMutation } from "@/src/infra/operations/useAppMutation";
 import { useRepository } from "@/src/infra/repositories/RepositoryProvider";
 import { useFeedbackService } from "@/src/services/feedbackService/FeedbackProvider";
+import { useMutation } from "@tanstack/react-query";
 import { useAuth } from "../AuthContext";
 import { AuthUser } from "../AuthUser";
 
@@ -9,8 +9,12 @@ export function useAuthSignIn() {
   const feedbackService = useFeedbackService();
   const { saveAuthUser } = useAuth();
 
-  return useAppMutation<AuthUser, { email: string; password: string }>({
-    mutateFn: ({ email, password }) => auth.signIn(email, password),
+  const { mutate, error, isPending } = useMutation<
+    AuthUser,
+    Error,
+    { email: string; password: string }
+  >({
+    mutationFn: ({ email, password }) => auth.signIn(email, password),
     onSuccess: (authUser) => {
       saveAuthUser(authUser);
       feedbackService.send({
@@ -22,8 +26,32 @@ export function useAuthSignIn() {
       feedbackService.send({
         type: "error",
         message: "Erro ao fazer login",
-        description: (error as Error).message ?? "Erro desconhecido",
+        description: error.message ?? "Erro desconhecido",
       });
     },
   });
+
+  return {
+    mutate,
+    error,
+    isPending,
+  };
+
+  // return useAppMutation<AuthUser, { email: string; password: string }>({
+  //   mutateFn: ({ email, password }) => auth.signIn(email, password),
+  //   onSuccess: (authUser) => {
+  //     saveAuthUser(authUser);
+  //     feedbackService.send({
+  //       type: "success",
+  //       message: `signed in: ${authUser.email}`,
+  //     });
+  //   },
+  //   onError: (error) => {
+  //     feedbackService.send({
+  //       type: "error",
+  //       message: "Erro ao fazer login",
+  //       description: (error as Error).message ?? "Erro desconhecido",
+  //     });
+  //   },
+  // });
 }
