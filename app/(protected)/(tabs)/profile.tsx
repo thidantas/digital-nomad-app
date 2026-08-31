c4781b1758f11c9b4109661bffdf94c250e969bf
thidantas <eng.thiagohpdantas@gmail.com>
2026-08-31 18:33:18 -0300

    feat(profile): show account info in ProfileHeader

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,24 +1,28 @@
 import { useAuthGetUser } from "@/src/domain/auth/operations/useAuthGetUser";
 import { useAuthSignOut } from "@/src/domain/auth/operations/useAuthSignOut";
 import { Box, Icon, Screen, Text } from "@/src/ui/components";
+import { ProfileHeader } from "@/src/ui/containers/Profile/ProfileHeader";
 import { Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ProfileScreen() {
   const { mutate: signOut } = useAuthSignOut();
 
-  const { data } = useAuthGetUser();
-
-  console.log({ data });
+  const { data: authUser } = useAuthGetUser();
 
   return (
     <Screen>
       <SafeAreaView>
-        <Text>Profile Screen</Text>
+        {authUser && <ProfileHeader authUser={authUser} />}
         <Pressable onPress={signOut}>
-          <Box flexDirection="row" alignItems="center">
-            <Text>Sair</Text>
-            <Icon name="Logout" color="primary" />
+          <Box
+            mt="s24"
+            flexDirection="row"
+            alignItems="center"
+            alignSelf="center"
+          >
+            <Icon name="Logout" color="fbErrorSurface" />
+            <Text color="fbErrorSurface">Sair</Text>
           </Box>
         </Pressable>
       </SafeAreaView>
