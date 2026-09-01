e5f021d3e47b9fcd16cbd8b49ffe02b51c513c81
thidantas <eng.thiagohpdantas@gmail.com>
2026-09-01 12:44:00 -0300

    fix(auth): clear query cache on sign out

--- src/domain/auth/operations/useAuthSignOut.ts
+++ src/domain/auth/operations/useAuthSignOut.ts
@@ -1,14 +1,18 @@
 import { useTanstackMutation } from "@/src/infra/operations/useTanstackMutation";
 import { useRepository } from "@/src/infra/repositories/RepositoryProvider";
+import { useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "../AuthContext";
 
 export function useAuthSignOut() {
   const { auth } = useRepository();
   const { removeAuthUser } = useAuth();
 
+  const queryClient = useQueryClient();
+
   return useTanstackMutation({
     mutationFn: () => auth.signOut(),
     onSuccess: () => {
+      queryClient.clear();
       removeAuthUser();
     },
   });
